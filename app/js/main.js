36cf2dbd9dd994263fe363aca6590b52808c3d94
sergey-komar <kursev81@yandex.ru>
2025-02-25 18:23:51 +0300

    home

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,33 @@ $(function () {
     $('.heading').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        dots: true
+        dots: true,
+        arrows: true,
+
+        responsive: [
+          {
+            breakpoint: 968,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true,
+              arrows: false
+            }
+          },
+         
+        ]
       });
+})
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector(".menu");
+    const mobile = document.querySelector(".nav-icon");
+
+    mobile.addEventListener("click", function () {
+      this.classList.toggle("nav-icon--active");
+      menu.classList.toggle("nav--active");
+    });
+
+    
 })
